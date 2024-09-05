2ccf7040a47e2586e5b78924aada1b1cee783d94
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-09-05 09:10:59 -0400

    Passed down props to the Content components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,6 @@
-function Content() {
-    return (<p>This is my first React Application, wish me luck!</p>);
+function Content(props) {
+    return (<p style={{color: props.color}}>{props.text}</p>);
   }
+  
 
 export default Content;
